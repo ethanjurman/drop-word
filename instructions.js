c36b958061525201ac1d25687d7ae3fec93364cb
Ethan Jurman <ethanjurman@gmail.com>
2022-03-06 01:41:19 -0500

    better instructions with gifs

--- instructions.js
+++ instructions.js
@@ -1,11 +1,19 @@
 const instructions = document.querySelector("#instructions");
 
-if (!localStorage.getItem(window.location.pathname)) {
+const currentDate = new Date().getTime();
+const lastOpenedDate = localStorage.getItem(window.location.pathname);
+
+// open instructions and set local storage if new
+if (!lastOpenedDate) {
   instructions.style.display = "block";
+  localStorage.setItem(window.location.pathname, currentDate);
 }
 
-// use localstorage to know if the instructions are open or not
-localStorage.setItem(window.location.pathname, 1);
+// open instructions and set local storage if over a day old
+if (currentDate - Number(lastOpenedDate) > 24 * 60 * 60 * 1000) {
+  instructions.style.display = "block";
+  localStorage.setItem(window.location.pathname, currentDate);
+}
 
 const instructionsOpenButton = document.createElement("div");
 instructionsOpenButton.classList.add("open-instructions");
